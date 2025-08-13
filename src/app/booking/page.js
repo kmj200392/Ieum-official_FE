494711942072d960283fe3de3acc9900f5eac71e
kmj200392 <kmj200392@gmail.com>
2025-08-13 12:48:04 +0900

    Header

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Footer from "../../components/OnboardingFooter";
 import styles from "./page.module.css";
 
 export default function BookingPage() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,18 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.headerContent}>
-                {/* 로고 */}
-                <div className={styles.logo}>
-                    <a href="/">
-                        <Image
-                            src="/방패 로고.png"
-                            alt="고려대학교 정보대학 로고"
-                            width={60}
-                            height={60}
-                            priority
-                        />
-                    </a>
-                </div>
-
-                {/* 사이드바 버튼 */}
-                <Sidebar />
+            <div className={styles.logo}>
+                <a href="/">
+                    <Image
+                        src="/header-logo.png"
+                        alt="고려대학교 로고"
+                        width={48}
+                        height={63}
+                        priority
+                    />
+                </a>
             </div>
+            <Sidebar />
         </header>
     );
 } 

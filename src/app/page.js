dab0b699107de906ff06cff19616d4229de00aa4
kmj200392 <kmj200392@gmail.com>
2025-08-14 20:25:51 +0900

    footer position

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Header from "../components/OnboardingHeader";
 import Footer from "../components/OnboardingFooter";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.canvas}>
+    <>
       <Header />
-      <div className={styles.spacer} />
       <Footer />
-    </div>
+    </>
   );
 }
